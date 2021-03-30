ca34afe0fcbe341298a89aa3d30c1608ac793a73
Enrique Hernandez <enrique_inf@hotmail.com>
2021-03-29 18:01:38 -0600

    adding method ServiceState

--- MyNewService/MyNewService.cs
+++ MyNewService/MyNewService.cs
@@ -8,6 +8,7 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Runtime.InteropServices;
 
 namespace MyNewService
 {
@@ -44,7 +45,36 @@ namespace MyNewService
 
         protected override void OnStop()
         {
+            eventLog1.WriteEntry("In OnStop");
         }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
+        }
+
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
+        };
 
         private void eventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
         {
